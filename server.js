4800b3b3a66a98d8fd378e1195d4432b5573a6d9
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-24 11:24:44 -0500

    Refactor server.js to streamline static file serving

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+app.use(express.static("public"));
+
 const PORT = process.env.PORT || 3000;
-
-// serve public folder
-app.use(express.static(path.join(__dirname, "public")));
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+app.listen(PORT, () => console.log("Running"));
