a63dd783032164aedfdab555c6a85e752a451089
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-24 08:48:13 -0500

    Refactor server.js to remove Express and add click handler
    Removed Express server setup and like functionality. Added click handling logic.

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-const express=require('express');const app=express();app.use(express.json());
+let clicked = false;
 
-let likes = { main: 0 };
-
-app.get('/',(req,res)=>res.sendFile(__dirname+'/index.html'));
-
-app.post('/like',(req,res)=>{
-  likes.main++;
-  res.json(likes);
-});
-
-app.get('/likes',(req,res)=>{
-  res.json(likes);
-});
-
-app.listen(3000,()=>console.log('RUNNING'));
+function handleClick() {
+  if (clicked) return;
+  clicked = true;
+  count++;
+  document.getElementById("count").innerText = count;
+}
