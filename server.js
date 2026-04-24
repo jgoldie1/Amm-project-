eadadc07f781d147c3aab920dc58c5877c49ea06
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-24 11:14:49 -0500

    Refactor server.js to use path for static files

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
-
-app.use(express.static("public")); // or your folder
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+
+// serve public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
