ad65fb06a9dcd14347a22a717b9bc145d4c04298
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-24 11:32:40 -0500

    Update static file serving and log message format

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-app.use(express.static("public"));
+
+// serve everything inside public folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Running"));
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
