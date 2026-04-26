2e08d0bcc649111bb3870b9ae4ee8d260b00b6eb
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-26 17:54:57 -0500

    Set up Express server to serve static files

--- server.js
+++ server.js
@@ -21,3 +21,19 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log("RUNNING ON PORT " + PORT);
 });
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log("RUNNING ON PORT " + PORT);
+});
