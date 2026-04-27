5b3cd41d543f3b456d2cccf25cbcdd4d609130e8
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-26 23:45:50 -0500

    Refactor server.js to manage moderators and user actions

--- server.js
+++ server.js
@@ -1,55 +1,47 @@
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+let mods = new Set();
+let raisedHands = [];
 
-app.use(express.static("public"));
-
-let users = {};
-let host = null;
-
-// CONNECTION
-io.on("connection", (socket) => {
-
-  socket.on("join", (username) => {
-    users[socket.id] = username;
-
-    // first user = host
-    if (!host) host = socket.id;
-
-    io.emit("userList", users);
-    socket.broadcast.emit("new-user", socket.id);
-  });
-
-  socket.on("disconnect", () => {
-    delete users[socket.id];
-
-    if (socket.id === host) {
-      host = Object.keys(users)[0] || null;
-    }
-
-    io.emit("userList", users);
-    socket.broadcast.emit("user-disconnected", socket.id);
-  });
-
-  // SIGNAL
-  socket.on("signal", (data) => {
-    io.to(data.to).emit("signal", {
-      from: socket.id,
-      signal: data.signal
-    });
-  });
-
-  // HOST CONTROLS
-  socket.on("kickUser", (id) => {
-    if (socket.id === host) {
-      io.to(id).emit("kicked");
-      io.sockets.sockets.get(id)?.disconnect();
-    }
-  });
-
+socket.on("makeMod", (id) => {
+  if (socket.id === host) mods.add(id);
 });
 
-http.listen(10000, () => {
-  console.log("RUNNING ON PORT 10000");
+socket.on("raiseHand", () => {
+  raisedHands.push({
+    id: socket.id,
+    name: users[socket.id]
+  });
+  io.emit("handList", raisedHands);
+});
+
+socket.on("approveJoin", (id) => {
+  if (socket.id === host || mods.has(socket.id)) {
+    io.to(id).emit("approved");
+  }
+});
+
+socket.on("chat", (msg) => {
+  io.emit("chat", {
+    id: socket.id,
+    user: users[socket.id],
+    text: msg
+  });
+});
+
+socket.on("gift", (amount) => {
+  io.emit("gift", {
+    user: users[socket.id],
+    amount: amount
+  });
+});
+
+socket.on("muteUser", (id) => {
+  if (socket.id === host || mods.has(socket.id)) {
+    io.to(id).emit("muted");
+  }
+});
+
+socket.on("unmuteUser", (id) => {
+  if (socket.id === host || mods.has(socket.id)) {
+    io.to(id).emit("unmuted");
+  }
 });
