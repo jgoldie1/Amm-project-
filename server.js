09621c7cca9726dd8386368aae77f7ace53763bf
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-26 23:36:07 -0500

    Implement host controls and user disconnection logic
    Added host management and user kick functionality.

--- server.js
+++ server.js
@@ -6,32 +6,46 @@ const io = require("socket.io")(http);
 app.use(express.static("public"));
 
 let users = {};
+let host = null;
 
-// SOCKET
+// CONNECTION
 io.on("connection", (socket) => {
 
   socket.on("join", (username) => {
     users[socket.id] = username;
 
-    // send full list
-    io.emit("userList", users);
+    // first user = host
+    if (!host) host = socket.id;
 
-    // tell others to auto-connect
+    io.emit("userList", users);
     socket.broadcast.emit("new-user", socket.id);
   });
 
   socket.on("disconnect", () => {
     delete users[socket.id];
+
+    if (socket.id === host) {
+      host = Object.keys(users)[0] || null;
+    }
+
     io.emit("userList", users);
     socket.broadcast.emit("user-disconnected", socket.id);
   });
 
-  // WEBRTC SIGNAL
+  // SIGNAL
   socket.on("signal", (data) => {
     io.to(data.to).emit("signal", {
       from: socket.id,
       signal: data.signal
     });
+  });
+
+  // HOST CONTROLS
+  socket.on("kickUser", (id) => {
+    if (socket.id === host) {
+      io.to(id).emit("kicked");
+      io.sockets.sockets.get(id)?.disconnect();
+    }
   });
 
 });
