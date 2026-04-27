5eba3bc223e31ef1420206320383b61227773d55
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-27 11:44:33 -0500

    Refactor chat handling by removing user tracking
    Removed user tracking and simplified chat message handling.

--- server.js
+++ server.js
@@ -8,25 +8,12 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-let users = {};
-
 io.on("connection", (socket) => {
 
-  socket.on("join", (name) => {
-    users[socket.id] = name || "anon";
-  });
-
   socket.on("chat", (msg) => {
-    if (typeof msg !== "string") return;
+    if (!msg) return;
 
-    const username = users[socket.id] || "anon";
-    const finalMessage = username + ": " + msg;
-
-    io.emit("chat", finalMessage);
-  });
-
-  socket.on("disconnect", () => {
-    delete users[socket.id];
+    io.emit("chat", msg);
   });
 
 });
