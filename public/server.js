5658445a94d073705a68b78853276a9386787054
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-28 00:59:27 -0500

    Refactor heart and gift increment logic

--- public/server.js
+++ public/server.js
@@ -1,9 +1,20 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
 let hearts = 0;
 let gifts = 0;
 
 io.on("connection", (socket) => {
   console.log("User connected");
 
+  // send correct values on join
   socket.emit("init", { hearts, gifts });
 
   socket.on("chat", (msg) => {
@@ -11,12 +22,16 @@ io.on("connection", (socket) => {
   });
 
   socket.on("heart", () => {
-    hearts++;
+    hearts += 1;
     io.emit("heart", hearts);
   });
 
   socket.on("gift", () => {
-    gifts++;
+    gifts += 1;
     io.emit("gift", gifts);
   });
+});
+
+server.listen(process.env.PORT || 10000, () => {
+  console.log("Server running");
 });
