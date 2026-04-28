6aece15272962da801d6b7192ed501662e790d29
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-27 23:55:25 -0500

    Refactor server.js to include package.json
    Removed server setup and socket.io functionality. Added package.json configuration.

--- server.js
+++ server.js
@@ -1,37 +1,11 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static("public"));
-
-let hearts = 0;
-let gifts = 0;
-
-io.on("connection", (socket) => {
-  console.log("User connected");
-
-  // send current counts when user joins
-  socket.emit("init", { hearts, gifts });
-
-  socket.on("chat", (msg) => {
-    io.emit("chat", msg);
-  });
-
-  socket.on("heart", () => {
-    hearts++;
-    io.emit("heart", hearts);
-  });
-
-  socket.on("gift", () => {
-    gifts++;
-    io.emit("gift", gifts);
-  });
-});
-
-server.listen(process.env.PORT || 10000, () => {
-  console.log("Server running");
-});
+{
+  "name": "amm-project",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
