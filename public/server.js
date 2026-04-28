c5e8f04926f51f8ff35ec057ad7044b4b935bfe4
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-28 07:41:02 -0500

    Integrate lowdb for persistent data storage
    Refactor server to use lowdb for persistent storage and simplify socket event handling.

--- public/server.js
+++ public/server.js
@@ -1,43 +1,41 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const { Low } = require("lowdb");
+const { JSONFile } = require("lowdb/node");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*" }
-});
+const io = new Server(server);
 
 app.use(express.static("public"));
 
-let hearts = 0;
-let gifts = 0;
+const db = new Low(new JSONFile("db.json"), { hearts: 0, gifts: 0 });
+async function initDB() {
+  await db.read();
+  db.data ||= { hearts: 0, gifts: 0 };
+}
+initDB();
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+io.on("connection", async (socket) => {
+  await db.read();
 
-  // ALWAYS send fresh values
-  socket.emit("init", { hearts, gifts });
+  socket.emit("init", db.data);
 
-  socket.on("chat", (msg) => {
-    io.emit("chat", msg);
+  socket.on("chat", (msg) => io.emit("chat", msg));
+
+  socket.on("heart", async () => {
+    db.data.hearts++;
+    await db.write();
+    io.emit("update", db.data);
   });
 
-  socket.on("heart", () => {
-    hearts++;
-    io.emit("update", { hearts, gifts });
-  });
-
-  socket.on("gift", () => {
-    gifts++;
-    io.emit("update", { hearts, gifts });
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User left:", socket.id);
+  socket.on("gift", async () => {
+    db.data.gifts++;
+    await db.write();
+    io.emit("update", db.data);
+    io.emit("explode");
   });
 });
 
-server.listen(process.env.PORT || 10000, () => {
-  console.log("Server running");
-});});
+server.listen(10000, () => console.log("Server running"));
