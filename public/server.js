26deaeb41aa48a6cb0cd706c6d8bd9ea3a444072
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-28 07:46:09 -0500

    Refactor server to use in-memory state instead of LowDB
    Replaced LowDB with in-memory state management for hearts and gifts. Updated socket events to use the new state structure.

--- public/server.js
+++ public/server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const { Low } = require("lowdb");
-const { JSONFile } = require("lowdb/node");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,32 +8,43 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-const db = new Low(new JSONFile("db.json"), { hearts: 0, gifts: 0 });
-async function initDB() {
-  await db.read();
-  db.data ||= { hearts: 0, gifts: 0 };
-}
-initDB();
+let state = {
+  hearts: 0,
+  gifts: 0,
+  users: {}
+};
 
-io.on("connection", async (socket) => {
-  await db.read();
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-  socket.emit("init", db.data);
+  // create simple profile
+  state.users[socket.id] = {
+    followers: Math.floor(Math.random() * 100)
+  };
 
-  socket.on("chat", (msg) => io.emit("chat", msg));
+  // send full state
+  socket.emit("init", state);
 
-  socket.on("heart", async () => {
-    db.data.hearts++;
-    await db.write();
-    io.emit("update", db.data);
+  socket.on("chat", (msg) => {
+    io.emit("chat", msg);
   });
 
-  socket.on("gift", async () => {
-    db.data.gifts++;
-    await db.write();
-    io.emit("update", db.data);
-    io.emit("explode");
+  socket.on("heart", () => {
+    state.hearts++;
+    io.emit("update", state);
+  });
+
+  socket.on("gift", () => {
+    state.gifts++;
+    io.emit("update", state);
+    io.emit("giftFX"); // trigger animation + sound
+  });
+
+  socket.on("disconnect", () => {
+    delete state.users[socket.id];
   });
 });
 
-server.listen(10000, () => console.log("Server running"));
+server.listen(process.env.PORT || 10000, () => {
+  console.log("Server running");
+});
