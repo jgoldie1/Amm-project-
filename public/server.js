d64295b879e60d38b47112af072a03651badeba8
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-28 00:46:17 -0500

    Add gifts functionality to server

--- public/server.js
+++ public/server.js
@@ -1,19 +1,10 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static("public"));
-
 let hearts = 0;
+let gifts = 0;
 
 io.on("connection", (socket) => {
   console.log("User connected");
 
-  socket.emit("init", hearts);
+  socket.emit("init", { hearts, gifts });
 
   socket.on("chat", (msg) => {
     io.emit("chat", msg);
@@ -23,8 +14,9 @@ io.on("connection", (socket) => {
     hearts++;
     io.emit("heart", hearts);
   });
-});
 
-server.listen(process.env.PORT || 10000, () => {
-  console.log("Server running");
+  socket.on("gift", () => {
+    gifts++;
+    io.emit("gift", gifts);
+  });
 });
