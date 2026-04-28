147b5655f54a7650fcff65998573fca653830969
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-27 23:29:53 -0500

    Set up Express server with static files and ping route

--- public/server.js
+++ public/server.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
 
+app.use(express.static("public"));
+
+app.get("/ping", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(process.env.PORT || 10000, () => {
+  console.log("Server running");
+});
