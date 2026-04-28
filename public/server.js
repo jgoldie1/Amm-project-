8d9394bdfa076f2c6743b764f658112c26c515dc
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-04-28 00:16:34 -0500

    Remove '/ping' endpoint from server
    Removed the '/ping' endpoint from the server.

--- public/server.js
+++ public/server.js
@@ -3,10 +3,6 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("/ping", (req, res) => {
-  res.send("ok");
-});
-
 app.listen(process.env.PORT || 10000, () => {
   console.log("Server running");
 });
