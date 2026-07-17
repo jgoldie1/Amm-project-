e96bccbb0ec6e59c89b98cbc16440b4c4c4f8971
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-07-17 15:04:47 -0500

    Add Africa Asia and Philippines launch codes

--- communityReferralData.js
+++ communityReferralData.js
@@ -1,7 +1,8 @@
 const initialReferralPartners = [
   ["James", "family"], ["Sarah", "family"], ["Jacobie", "family"], ["Isaiah", "family"], ["Aniyah", "family"], ["Al", "family"], ["Kevon", "family"], ["Don", "family"], ["Carlton", "family"], ["Kenny", "family"], ["Mike", "family"], ["Shawndell", "family"], ["Ashley", "family"], ["Delvell", "family"], ["Keshawn", "family"], ["Ashley 2", "family"],
   ["Niki", "community"], ["BJ", "community"], ["Toya", "community"], ["Sheanes", "community"], ["Latasha", "community"], ["Dea", "community"], ["De Angel", "community"], ["Dawn", "community"], ["Amy", "community"], ["Tasha", "community"], ["Adrian", "family"], ["Flexis", "community"], ["Natasha", "community"], ["Kya", "community"], ["Soraya", "community"], ["Leandra", "community"], ["Day Day", "community"], ["Solomand", "community"], ["Golden Ma", "community"], ["Take Monroe", "community"], ["Pndand Mt", "community"],
-  ["TikTok Fam", "tiktok"], ["BIGO Fam", "bigo"]
+  ["TikTok Fam", "tiktok"], ["BIGO Fam", "bigo"],
+  ["Africa", "africa"], ["Asia", "asia"], ["Philippines", "philippines"]
 ];
 
 module.exports = { initialReferralPartners };
