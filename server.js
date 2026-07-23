d82e66085b2173e8ba789833469cd38d52518771
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-07-23 16:06:51 -0500

    Wire OmniNews Oracle into server

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { createFreeTvManager } = require("./lib/free-tv-manager");
 const { registerFreeTvRoutes } = require("./lib/free-tv-routes");
 const { createNewsManager } = require("./lib/news-manager");
 const { registerNewsRoutes } = require("./lib/news-routes");
+const { createOmniNewsOracleManager } = require("./lib/omni-news-oracle-manager");
+const { registerOmniNewsOracleRoutes } = require("./lib/omni-news-oracle-routes");
 const HOLO_MENU = require("./data/holo-menu.json");
 
 const app = express();
@@ -13,12 +15,14 @@ const server = http.createServer(app);
 const io = new Server(server);
 const freeTv = createFreeTvManager();
 const news = createNewsManager();
+const omniNewsOracle = createOmniNewsOracleManager();
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static("public"));
 registerHolo5dxRoutes({ app });
 registerFreeTvRoutes({ app, manager: freeTv });
 registerNewsRoutes({ app, manager: news });
+registerOmniNewsOracleRoutes({ app, manager: omniNewsOracle });
 app.get('/api/holo-menu', (_req, res) => res.json(HOLO_MENU));
 
 let hearts = 0;
