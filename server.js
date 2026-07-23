78e5b8cea5b11608a9942b18a2ec39957d104ed9
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-07-23 14:54:00 -0500

    Wire Holo5DX Core v0.2 routes into server

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const { registerHolo5dxRoutes } = require("./lib/holo5dx-routes");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.json({ limit: "1mb" }));
 app.use(express.static("public"));
+registerHolo5dxRoutes({ app });
 
 let hearts = 0;
 let gifts = 0;
