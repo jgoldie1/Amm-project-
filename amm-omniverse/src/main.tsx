5e0a30c2ae58bb0c37a6e8163c937459b89a4789
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-08-16 08:27:35 -0500

    feat: mount universal safety launcher

--- amm-omniverse/src/main.tsx
+++ amm-omniverse/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import UniversalSafetyLauncher from './components/UniversalSafetyLauncher'
 import { installProductionHealthMonitor } from './runtime/ProductionHealthMonitor'
 
 installProductionHealthMonitor()
@@ -8,5 +9,6 @@ installProductionHealthMonitor()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <UniversalSafetyLauncher />
   </StrictMode>
 )
