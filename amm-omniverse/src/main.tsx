7e717b0974545034400052ef4780b15855bbc5de
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-08-16 16:38:37 -0500

    Mount GameVerse globally with TRYAMM shell

--- amm-omniverse/src/main.tsx
+++ amm-omniverse/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import UniversalSafetyLauncher from './components/UniversalSafetyLauncher'
 import MiddleverseLauncher from './components/MiddleverseLauncher'
+import GameVerseLauncher from './components/GameVerseLauncher'
 import { installProductionHealthMonitor } from './runtime/ProductionHealthMonitor'
 
 installProductionHealthMonitor()
@@ -12,5 +13,6 @@ createRoot(document.getElementById('root')!).render(
     <App />
     <UniversalSafetyLauncher />
     <MiddleverseLauncher />
+    <GameVerseLauncher />
   </StrictMode>
 )
