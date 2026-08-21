f9abf5a8d9ce74ae3b739396b857567cbf03a856
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-08-21 01:39:24 -0500

    Mount sample rights center globally

--- amm-omniverse/src/main.tsx
+++ amm-omniverse/src/main.tsx
@@ -13,6 +13,7 @@ import StreetVerseLifePathLauncher from './components/StreetVerseLifePathLaunche
 import StreetVerseExpansionHub from './components/StreetVerseExpansionHub'
 import TryAMMMobileLauncher from './components/TryAMMMobileLauncher'
 import HoloArenaLauncher from './components/HoloArenaLauncher'
+import SampleRightsCenter from './components/SampleRightsCenter'
 import './accessibility/accessibility.css'
 import { installProductionHealthMonitor } from './runtime/ProductionHealthMonitor'
 
@@ -33,5 +34,6 @@ createRoot(document.getElementById('root')!).render(
     <StreetVerseExpansionHub />
     <TryAMMMobileLauncher />
     <HoloArenaLauncher />
+    <SampleRightsCenter />
   </StrictMode>
 )
