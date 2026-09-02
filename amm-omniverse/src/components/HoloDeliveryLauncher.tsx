7f9568f8504e70123dc3c836bb89a1e36b61d610
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-09-01 20:14:54 -0500

    feat: connect Holo Delivery to StreetVerse shopping missions

--- amm-omniverse/src/components/HoloDeliveryLauncher.tsx
+++ amm-omniverse/src/components/HoloDeliveryLauncher.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import HoloDeliveryCenter from './HoloDeliveryCenter';
+import { installStreetVerseShoppingDeliveryMissionRuntime } from '../runtime/StreetVerseShoppingDeliveryMissionRuntime';
+
+installStreetVerseShoppingDeliveryMissionRuntime();
 
 export default function HoloDeliveryLauncher() {
   const [open, setOpen] = useState(false);
