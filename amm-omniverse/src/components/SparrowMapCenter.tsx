a0ddc9a4412ffdba5e00a5563000d9081edcdbf0
jgoldie1 <77651266+jgoldie1@users.noreply.github.com>
2026-09-24 10:38:30 -0500

    fix: use MapLibre v6 ESM namespace import

--- amm-omniverse/src/components/SparrowMapCenter.tsx
+++ amm-omniverse/src/components/SparrowMapCenter.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useMemo, useRef, useState } from 'react'
-import maplibregl, { type Map as MapLibreMap } from 'maplibre-gl'
+import * as maplibregl from 'maplibre-gl'
+import type { Map as MapLibreMap } from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 
 type LayerId = 'business'|'mobility'|'environment'|'missions'|'infrastructure'|'alerts'
